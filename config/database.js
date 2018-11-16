f7aa253db9caaff1262bfd860667ea748713eb23
Muzzamil75 <42649485+Muzzamil75@users.noreply.github.com>
2018-11-16 18:26:41 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://CHANGEME'}
+  module.exports = {mongoURI: mongodb://<user>:<dbpass1>@ds039291.mlab.com:39291/crud_db}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
